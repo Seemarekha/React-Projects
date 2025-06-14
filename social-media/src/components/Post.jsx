ab7b8bb872cb52f29c178069b8598ddc9d640362
Seemarekha Behera <129779011+Seemarekha@users.noreply.github.com>
2025-06-14 07:29:27 +0000

    ..

--- social-media/src/components/Post.jsx
+++ social-media/src/components/Post.jsx
@@ -1,10 +1,15 @@
-const Post = ({post}) => {
+const Post = ({ post }) => {
     return (
         <div className="card post-card" style={{ width: "18rem" }}>
             <div className="card-body">
-                <h5 className="card-title">{post.title}</h5>
+                <h5 className="card-title">{post.title}
+                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        {post.reactions}
+                    </span>
+                </h5>
                 <p className="card-text">{post.body}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                {post.tags.map(tag => <span class="badge text-bg-primary hashtag">{tag}</span>)}
+
             </div>
         </div>
     )
