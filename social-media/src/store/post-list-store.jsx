853deddfaf4b0f6fd5c5085192420116326fa983
Seemarekha Behera <129779011+Seemarekha@users.noreply.github.com>
2025-06-17 09:21:01 +0000

    ...

--- social-media/src/store/post-list-store.jsx
+++ social-media/src/store/post-list-store.jsx
@@ -12,6 +12,10 @@ const postListReducer = (currentPostList, action) => {
     if (action.type === 'DELETE_POST') {
         newPostList = currentPostList.filter(post => post.id !== action.payload.postId)
     }
+    else if (action.type === 'ADD_POST') {
+        newPostList = [action.payload, ...currentPostList]
+
+    }
     return newPostList;
 }
 
@@ -19,7 +23,21 @@ const PostListProvider = ({ children }) => {
 
     const [postList, dispatchPostList] = useReducer(postListReducer, DEFAULT_POST_LIST);
 
-    const addPost = () => {
+    const addPost = (userId, postTitle, postBody, reactions, tags) => {
+
+        dispatchPostList({
+            type: 'ADD_POST',
+            payload: {
+                id: Date.now(),
+                title: postTitle,
+                body: postBody,
+                reactions: reactions,
+                userId: userId,
+                tags: tags,
+
+            }
+        })
+
 
     }
     const deletePost = (postId) => {
