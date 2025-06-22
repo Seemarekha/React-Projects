61db2f8ff8001001aecb89c4e4303c2d7e25a992
Seemarekha Behera <129779011+Seemarekha@users.noreply.github.com>
2025-06-22 11:35:20 +0000

    loading

--- social-media/src/components/PostList.jsx
+++ social-media/src/components/PostList.jsx
@@ -1,16 +1,21 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import WelcomeMsg from "./WelcomeMsg";
+import LoadingSpinner from "./LoadingSpinner";
 
 const PostList = () => {
     const { postList, addInitialPosts } = useContext(PostListData);
 
+    const [fetching, setFetching] = useState(false);
+
     useEffect(() => {
+        setFetching(true);
         fetch('https://dummyjson.com/posts')
             .then(res => res.json())
             .then(data => {
                 addInitialPosts(data.posts);
+                setFetching(false);
             });
 
     }, [])
@@ -20,9 +25,12 @@ const PostList = () => {
     return (
         <>
             {
-                postList.length === 0 && <WelcomeMsg />
+              fetching && <LoadingSpinner />
             }
-            {postList.map((post) => (
+            {
+                !fetching && postList.length === 0 && <WelcomeMsg />
+            }
+            {  !fetching && postList.map((post) => (
                 <Post key={post.id} post={post} />
             ))}
 
