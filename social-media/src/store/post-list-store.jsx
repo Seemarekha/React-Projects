6fdbaaf6681b07440896ee2c892cb34c5e915544
Seemarekha Behera <129779011+Seemarekha@users.noreply.github.com>
2025-06-24 14:05:10 +0000

    useCallback

--- social-media/src/store/post-list-store.jsx
+++ social-media/src/store/post-list-store.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useCallback, useReducer } from "react";
 
 
 export const PostList = createContext({
@@ -54,15 +54,17 @@ const PostListProvider = ({ children }) => {
         });
     };
 
-    const deletePost = (postId) => {
+    const deletePost = useCallback(
+        (postId) => {
         dispatchPostList({
             type: 'DELETE_POST',
             payload: {
                 postId,
-            }
-        })
-
-    }
+            },
+        });
+     },
+     [dispatchPostList]
+);
 
     return (
         <PostList.Provider value={
